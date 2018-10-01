0e02b1f937863c77a5649c6226d308bc2f6d4e90
Ajo Fod <ajo.fod@gmail.com>
2018-10-01 14:16:52 -0700

    Blast Off

--- src/main/java/com/mycompany/app/Run.java
+++ src/main/java/com/mycompany/app/Run.java
@@ -4,7 +4,9 @@ import org.apache.commons.io.FileUtils;
 
 public class Run {
     public static void main(String[] args) {
+        System.out.println("Blast Off");
         System.out.println(System.getProperty("java.class.path"));
         System.out.println("Temp Dir: "+FileUtils.getTempDirectoryPath());
+
     }
 }
