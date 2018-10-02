0997d873a054bffd4c1084d1b9422e112f744b41
Ajo Fod <ajo.fod@gmail.com>
2018-10-02 13:35:47 -0700

    Using Jetty for server

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,18 +1,27 @@
 package com.mycompany.app;
 
 
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+
 /**
  * Hello world!
  */
-public class App
-{
+public class App {
+
 
     private final String message = "Hello World!";
 
-    public App() {}
+    public App(){};
 
     public static void main(String[] args) {
-        System.out.println(new App().getMessage());
+        try {
+            System.out.println(new App().getMessage());
+        } catch (Exception ex) {
+            throw new RuntimeException("Boo");
+        }
     }
 
     private final String getMessage() {
--- src/main/java/com/mycompany/app/Run.java
+++ src/main/java/com/mycompany/app/Run.java
@@ -1,12 +1,37 @@
 package com.mycompany.app;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+
 import org.apache.commons.io.FileUtils;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
 
-public class Run {
-    public static void main(String[] args) {
-        System.out.println("Blast Off");
-        System.out.println(System.getProperty("java.class.path"));
-        System.out.println("Temp Dir: "+FileUtils.getTempDirectoryPath());
+public class Run extends AbstractHandler
+{
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException, ServletException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
+        System.out.println("Temp Dir: " + FileUtils.getTempDirectoryPath());
+    }
 
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(9999);
+        server.setHandler(new Run());
+
+        server.start();
+        server.join();
     }
 }
